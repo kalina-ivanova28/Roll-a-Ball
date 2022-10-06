6cf3badbba3687104fa2bb86e943778c616a9781
Miguel Divo <migueldivo11@gmail.com>
2022-10-06 21:54:05 +0100

    full game

--- Roll-a-ball/Assets/PlayerController.cs
+++ Roll-a-ball/Assets/PlayerController.cs
@@ -8,10 +8,15 @@ public class PlayerController : MonoBehaviour {
 public Vector2 moveValue;
 public float speed;
 private int count;
+private int numPickups = 4; // Put here the number of pickups you have .
+public TMPro.TextMeshProUGUI ScoreText;
+public TMPro.TextMeshProUGUI WinText;
 
     void Start()
     {
         count=0;
+        WinText.text = "";
+        SetCountText();
     }
     void OnMove ( InputValue value ) {
         moveValue = value.Get< Vector2 >() ;
@@ -27,7 +32,17 @@ private int count;
         if( other.gameObject.tag == "PickUp" ){
             other.gameObject.SetActive(false);
             count++;
+            SetCountText();
         }
     }
+
+    private void SetCountText()
+    {
+        ScoreText.text = " Score : " + count.ToString();
+        if (count >= numPickups)
+        {
+            WinText.text = " You win ! ";
+            }
+        }
 
 }
